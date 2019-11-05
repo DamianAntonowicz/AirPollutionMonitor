b2e32e47ad58d6da32e7073488c89ec7e1c5a2ce
Damian Antonowicz <kontakt@damianantonowicz.pl>
2019-11-05 17:51:33 +0100

    Initial commit of default Shell app.

---
+++ AirPollutionMonitor/AirPollutionMonitor/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AirPollutionMonitor.Models;
+
+namespace AirPollutionMonitor.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
